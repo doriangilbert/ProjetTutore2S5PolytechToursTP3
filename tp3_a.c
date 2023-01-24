39a69a593dff27232a83bf1695d431a5259bfe7a
doriangilbert <dodo23082001@gmail.com>
2023-01-24 20:44:16 +0100

    Poursuite de l'analyseur LR, implémentation des différentes tables

--- tp3_a.c
+++ tp3_a.c
@@ -1,6 +1,6 @@
 /**
  * \file tp3_a.c
- * \brief Table LR
+ * \brief Analyseur syntaxique LR
  * \author GILBERT Dorian et DHONDT Matheo
  * \version 0.1
  * \date 20/01/2023
@@ -13,43 +13,125 @@
 #include "pile.h"
 #include "tp2_lex.h"
 
-int analyseurLR(){
-   /* initialisation */
+/* Table contenant les actions à effectuer (Acc='a' ; Deplacement='d' ; Reduction='<un numéro de règle>' ; Erreur='e') */
+char *TableAction[26][18] =
+/*{    }    [    ]    ,    :    s    n    t    f    u    #    O    M    P    A    E    V*/
+{
+{'d', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //0
+{'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'a', 'e', 'e', 'e', 'e', 'e', 'e'}, //1
+{'e', 'd', 'e', 'e', 'e', 'e', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //2
+{'e', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //3
+{'e', '3', 'e', 'e', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //4
+{'e', '1', 'e', '1', '1', 'e', 'e', 'e', 'e', 'e', 'e', '1', 'e', 'e', 'e', 'e', 'e', 'e'}, //5
+{'e', 'e', 'e', 'e', 'e', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //6
+{'e', '2', 'e', '2', '2', 'e', 'e', 'e', 'e', 'e', 'e', '2', 'e', 'e', 'e', 'e', 'e', 'e'}, //7
+{'e', 'e', 'e', 'e', 'e', 'e', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //8
+{'d', 'e', 'd', 'e', 'e', 'e', 'd', 'd', 'd', 'd', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //9
+{'e', '4', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //10
+{'e','12', 'e','12','12', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //11
+{'e','13', 'e','13','13', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //12
+{'e', '5', 'e', 'e', '5', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //13
+{'d', 'e', 'd', 'd', 'e', 'e', 'd', 'd', 'd', 'd', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //14
+{'e','10', 'e','10','10', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //15
+{'e','11', 'e','11','11', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //16
+{'e','14', 'e','14','14', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //17
+{'e','15', 'e','15','15', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //18
+{'e','16', 'e','16','16', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //19
+{'e', 'e', 'e', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //20
+{'e', 'e', 'e', '8', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //21
+{'e', '6', 'e', '6', '6', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //22
+{'e', '7', 'e', '7', '7', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //23
+{'d', 'e', 'd', 'e', 'e', 'e', 'd', 'd', 'd', 'd', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}, //24
+{'e', 'e', 'e', '9', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'}};//25
+
+/* Table contenant les états vers lesquels naviguer (Erreur/Pas d'état=-1) */
+int TableGoto[26][18] =
+/*{  }  [  ]  ,  :  s  n  t  f  u  #  O  M  P  A  E  V*/
+{
+{ 2,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1,-1,-1,-1,-1,-1}, //0
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 0,-1,-1,-1,-1,-1,-1}, //1
+{-1, 5,-1,-1,-1,-1, 6,-1,-1,-1,-1,-1,-1, 3, 4,-1,-1,-1}, //2
+{-1, 7,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //3
+{-1,-1,-1,-1, 8,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //4
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //5
+{-1,-1,-1,-1,-1, 9,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //6
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //7
+{-1,-1,-1,-1,-1,-1, 6,-1,-1,-1,-1,-1,-1,10, 4,-1,-1,-1}, //8
+{ 2,-1,14,-1,-1,-1,15,16,17,18,19,-1,11,-1,-1,12,-1,13}, //9
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //10
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //11
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //12
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //13
+{ 2,-1,14,22,-1,-1,15,16,17,18,19,-1,11,-1,-1,12,20,21}, //14
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //15
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //16
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //17
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //18
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //19
+{-1,-1,-1,23,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //20
+{-1,-1,-1,-1,24,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //21
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //22
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //23
+{ 2,-1,14,-1,-1,-1,15,16,17,18,19,-1,11,-1,-1,12,25,21}, //24
+{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}};//25
+
+/* Table contenant la taille de la partie droite de chaque règle*/
+int *taillePartieDroiteRegle = {2,3,1,3,3,2,3,1,3,1,1,1,1,1,1,1};
+
+/* Table contenant l'élément en partie gauche de chaque règle*/
+char *elementPartieGaucheRegle = {'O','O','M','M','P','A','A','E','E','V','V','V','V','V','V','V'};
+
+/**
+ * \fn int analyseurLR()
+ * \brief fonction qui fait l'analyse LR
+ *
+ * \param[in] _data chaine à analyser
+ * \param[out] _valide boolen indiquant la validité de la chaine passé en paramètre vis a vis de la grammaire JSON, 1 si valide, 0 sinon
+ */
+int analyseurLR(char *_chaine){
+/* initialisation */
 TIntPile *_pile=initIntPile();
 TVoidPile *_pileJSon=initVoidPile();
 empilerInt(_pile,0);
 /* on recupère un symbole en appelant l'analyseur lexicale */
-int lexeme = Lex();
+TLex *lex_data_analyseur = NULL;
+lex_data_analyseur = initLexData(_chaine); //NE PAS OUBLIER DE FAIRE deleteLexData(lex_data_analyseur) à la fin
+int lexeme = lex(lex_data_analyseur);
 /* boucle générale */
 int fini = 0;
 while (!fini){
     int action;
-    action = TableAction[sommet(_pile)][lexeme]; /* Acc=0 ; Deplacement=1 ; Reduction=2*/
-    if (action = 'a')
+    action = TableAction[sommetInt(_pile)][lexeme]; /* Acc='a' ; Deplacement='d' ; Reduction='r' ; Erreur='e' */
+    if (action == 'a')
     {
         /*O = Depiler un objet JSon de la PileJSon /* l'arbre JSON est sur la pile *Fini = vrai
         Retourner (O)*/
         return 0;
     }
-    else if (action ='d')
+    else if (action == 'd')
     {
-        empilerInt(_pile,TableGoto[sommet(_pile)][lexeme]);
-        lexeme = Lex();
+        empilerInt(_pile,TableGoto[sommetInt(_pile)][lexeme]);
+        int lexeme = lex(lex_data_analyseur);
     }
-    else if (action ='e')
+    else if (action == 'e')
     {
         printf("ERREUR\n");
         fini=1;
     }
     else
     {
+        int action = (int)action;
         int i=0;
         while (i<taillePartieDroiteRegle[action])
         {
             depilerInt(_pile);
             i++;
         }
-        empilerInt(TableGoto[sommet(Pile)][ElementPartieGaucheRegle[action]]);
+        empilerInt(_pile,TableGoto[sommetInt(_pile)][elementPartieGaucheRegle[action]]);
     }
 }
+}
+
+void main(int argc, char *argv[]) {
+    //A COMPLETER avec la lecture de fichier, l'appel à l'analyseur LR et la libération des ressources
 }
